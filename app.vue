3cde481ccff80d2e82f92768ecc511186a045a4b
yizhe <liaoyizhe75@gmail.com>
2024-07-11 10:05:31 +0800

    test og:type

--- app.vue
+++ app.vue
@@ -17,6 +17,7 @@ useHead({
       content:
         '兆豐輪胎保養廠是一家專業的汽車保養與輪胎服務公司。聯繫資訊：地址：台中市北屯區昌平路三段179號。電話：(04) 24220080。'
     },
+    { property: 'og:type', content: 'website' },
     { property: 'og:title', content: '兆豐輪胎保養廠 - 專業的汽車保養與輪胎服務' },
     { property: 'og:image', content: 'https://megatire.net/images/serve.png' },
     { property: 'og:url', content: 'https://megatire.net' },
